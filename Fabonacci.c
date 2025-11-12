c9b08dd263f2b43d8287effe5714ee62998ffb3d
YASH RAJPUT <yash34686@gmail.com>
2025-11-12 21:56:32 +0530

    No changes At ALL

---
+++ Fabonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the value : ");
+    scanf("%d",&n);
+    int a = 0, b = 1, c;
+    printf("Fibonacci Series: %d %d ", a, b);
+    for(int i = 2; i < n; i++) {
+        c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+    return 0;
+}
